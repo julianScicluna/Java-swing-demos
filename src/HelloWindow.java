d8dda0b878054def6e4b90db2c451b737d474e7e
julianScicluna <108342409+julianScicluna@users.noreply.github.com>
2023-12-09 21:32:42 +0100

    Update HelloWindow.java

--- src/HelloWindow.java
+++ src/HelloWindow.java
@@ -2,9 +2,12 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
 
 import java.awt.FlowLayout;
 import java.awt.Color;
+
+import java.lang.reflect.InvocationTargetException;
 
 public class HelloWindow {
     public static void main(String[] args) {
